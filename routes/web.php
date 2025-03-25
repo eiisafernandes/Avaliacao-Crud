18e5266be89d6b3d3edc0ca203f96496fce7016e
eiisafernandes <isaafernandes2018@gmail.com>
2025-03-25 15:03:31 -0300

    atualização

--- routes/web.php
+++ routes/web.php
@@ -1,23 +1,6 @@
 <?php
 
 use App\Livewire\Cliente\Cadastro\Cadastro;
-use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
-//CADASTRO DE CLIENTES
 Route::get('cadastro', Cadastro::class);
-
-
-//ROTAS DOS 3 LOGINS
-
-
-//PAGINA DE LOGIN
-Route::get('login', Login::class);
-
-//PAGINA INICIAL CLIENTE
-Route::get('pagina/inicial/cliente', Dashboard::class)->name('cliente.dashboard.dashboard');
-
-Route::get('/aluno/pagina', function (){
-    return redirect(route('cliente.dashboard.dashboard'));
-})->middleware(['auth', 'role:aluno'])->name('cliente.dashboard');
